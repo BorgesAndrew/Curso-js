dcacbb5a2fd712fa7aaee4dfacab7e04784591d9
BorgesAndrew <andrew.borges@avantis.edu.br>
2018-09-17 11:00:55 -0300

    Array - Filter, Com regExp para Pesquisa

---
+++ js/pesquisa.js
@@ -0,0 +1,9 @@
+let nome = ['Maria','João','Pedro','José','Flávio','Fernanda','Marta'];
+
+let pesquisa = prompt ("Pesquisa: ");
+
+let regExp = new RegExp(pesquisa,'ig');
+
+let resultado = nome.filter(valor=>regExp.test(valor));
+
+alert(resultado);
